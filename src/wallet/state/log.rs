fd5a1a8450dfb9b53a07d4010a0d585cc020e03e
Mikhail Zabaluev <mikhail.zabaluev@iohk.io>
2018-11-07 14:37:04 +0200

    Fix a typo in method name

--- src/wallet/state/log.rs
+++ src/wallet/state/log.rs
@@ -114,7 +114,7 @@ impl<A: serde::Serialize> Log<A> {
 impl<A> Log<A>
     where for<'de> A: serde::Deserialize<'de>
 {
-    fn deserisalise(bytes: &[u8]) -> Result<Self> {
+    fn deserialize(bytes: &[u8]) -> Result<Self> {
         let mut reader = bytes;
 
         {
@@ -234,7 +234,7 @@ impl LogReader {
         match self.0.next()? {
             None => Ok(None),
             Some(bytes) => {
-                let log = Log::deserisalise(&bytes)?;
+                let log = Log::deserialize(&bytes)?;
                 Ok(Some(log))
             }
         }
